f82e95a5336526af9169c3c3bea58fda91523f83
HagiwaraNaoki <ddt.nk.hagiwara@gmail.com>
2024-04-04 17:06:47 +0900

    [Change]description

--- exec02/Exercise2_2.java
+++ exec02/Exercise2_2.java
@@ -16,11 +16,11 @@ public class Exercise2_2 {
 	 * 作成日:2024/04/01
 	 */
 	public static void main(String[] args) {
-		// 一つ目の数字を定義し、firstNumberに63を代入
+		// 一つ目の数字を定義し、一つ目の整数に63を代入
 		int firstNumber = 63;
-		// 二つ目の数字を定義し、secondNumberに18を代入
+		// 二つ目の数字を定義し、二つ目の整数に18を代入
 		int secondNumber = 18;
-		// 三つ目の数字を定義し、thirdNumberに50を代入
+		// 三つ目の数字を定義し、三つ目の整数に50を代入
 		int thirdNumber = 50;
 		
 		// 三つの値の合計値をコンソールに出力
