961c5b257e05d7d5c0e6a2f4e56c1db1ba22e031
HagiwaraNaoki <ddt.nk.hagiwara@gmail.com>
2024-04-05 09:40:52 +0900

    [Change] 3_2,3_8,3_12

--- exec03/Exercise3_12.java
+++ exec03/Exercise3_12.java
@@ -18,35 +18,37 @@ public class Exercise3_12 {
 	 * 作成日:2024/04/01
 	 */
 	public static void main(String[] main) {
-		// Scannerクラスの変数を定義
-		Scanner standardInput = new Scanner(System.in);
-		
-		// 文字列を改行なしで出力
-		System.out.print("一つ目の整数：");
-		// 一つ目の整数の値をコンソールで受け付け
-		int firstNumber = standardInput.nextInt();
-		// 文字列を改行なしで出力
-		System.out.print("二つ目の整数：");
-		// 二つ目の整数の値をコンソールで受け付け
-		int secondNumber = standardInput.nextInt();
-		// 文字列を改行なしで出力
-		System.out.print("三つ目の整数：");
-		// 三つ目の整数の値をコンソールで受け付け
-		int thirdNumber = standardInput.nextInt();
-		
-		//int型の変数minimumValueにaを代入
-		int minimumValue = firstNumber;
-		// bよりもminimumValueが大きいとき
-		if (secondNumber < minimumValue)
-			// minimumValueにbを代入
-			minimumValue = secondNumber;
-		// cよりもminimumValueが大きいとき
-		if (thirdNumber < minimumValue)
-			// minimumValueにcを代入
-			minimumValue = thirdNumber;
-		// 最小値を出力
-		System.out.println("最小値は" + minimumValue + "です。");
-		// standardInputの受付を終了
-		standardInput.close();
-	}
+    // Scannerクラスの変数を定義
+    Scanner standardInput = new Scanner(System.in);
+
+    // 文字列を改行なしで出力
+    System.out.print("一つ目の整数：");
+    // 一つ目の整数の値をコンソールで受け付け
+    int firstNumber = standardInput.nextInt();
+    // 文字列を改行なしで出力
+    System.out.print("二つ目の整数：");
+    // 二つ目の整数の値をコンソールで受け付け
+    int secondNumber = standardInput.nextInt();
+    // 文字列を改行なしで出力
+    System.out.print("三つ目の整数：");
+    // 三つ目の整数の値をコンソールで受け付け
+    int thirdNumber = standardInput.nextInt();
+
+    // 最小値に最初の整数を代入
+    int minimumValue = firstNumber;
+    // 最小値よりも二つ目の整数が小さいとき
+    if (secondNumber < minimumValue) {
+      // 最小値に二つ目の整数を代入
+      minimumValue = secondNumber;
+    }
+    // 最小値よりも三つ目の整数が小さいとき
+    if (thirdNumber < minimumValue) {
+      // 最小値に三つ目の整数を代入
+      minimumValue = thirdNumber;
+    }
+    // 最小値を出力
+    System.out.println("最小値は" + minimumValue + "です。");
+    // standardInputの受付を終了
+    standardInput.close();
+  }
 }
