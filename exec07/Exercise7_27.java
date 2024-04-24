a98d41c71c95f086a7f66b55e9a5c964c61fb476
HagiwaraNaoki <ddt.nk.hagiwara@gmail.com>
2024-04-24 10:15:18 +0900

    7-27

--- exec07/Exercise7_27.java
+++ exec07/Exercise7_27.java
@@ -16,226 +16,180 @@ public class Exercise7_27 {
 	 * 関数名:addMatrix
 	 * 概要:３つの配列の要素数が等しければ加算を行いtrueを返す
 	 * 等しくなければ加算を行わずにfalseを返す
-	 * また、加算結果が正しいかどうかも判定し、結果をメッセージで出力する
 	 * 引数:受け取った配列を格納するint型配列[x[][]],[y[][]],[z[][]]
 	 * 戻り値:加算の成功/失敗を示すboolean型の値
 	 * 作成者:N.Hagiwara
 	 * 作成日:2024/04/08
 	 */
 	static boolean addMatrix(int[][] x, int[][] y, int[][] z) {
-		// 配列の要素数を計算
-		int xElements = x.length * x[0].length;
-		// 配列の要素数を計算
-		int yElements = y.length * y[0].length;
-		// 配列の要素数を計算
-		int zElements = z.length * z[0].length;
+		// 加算が可能かどうかを示すフラグ
+		boolean canAdd = true;
 
-		// 配列の要素数が等しいかどうかを示すフラグ変数
-		boolean equalElements = true;
-		// 加算結果が正しいかどうかを示すフラグ変数
-		boolean additionCorrect = true;
-
-		// 配列の要素数が等しいか確認
-		if (xElements == yElements && xElements == zElements) {
-			// 配列の要素数が等しい場合、加算を行う
-			int arrayIndex = 0;
-			// 配列xの行数分繰り返す
-			for (int i = 0; i < x.length; i++) {
-				// 配列xの列数分繰り返す
-				for (int j = 0; j < x[i].length; j++) {
-					// 配列zの要素を、配列xとyの対応する要素の和に設定
-					z[arrayIndex / z[0].length][arrayIndex % z[0].length] = x[i][j] + y[arrayIndex / y[0].length][arrayIndex % y[0].length];
-					// インデックスを増加
-					arrayIndex++;
-				}
-			}
-
-			// インデックスを初期化
-			arrayIndex = 0;
-			// 配列xの行数分繰り返す
-			for (int i = 0; i < x.length; i++) {
-				// 配列xの列数分繰り返す
-				for (int j = 0; j < x[i].length; j++) {
-					// 配列zの要素が、配列xとyの対応する要素の和と等しくない場合
-					if (z[arrayIndex / z[0].length][arrayIndex % z[0].length] != x[i][j] + y[arrayIndex / y[0].length][arrayIndex % y[0].length]) {
-						// 加算結果が正しくないことを示すフラグ変数をfalseに設定
-						additionCorrect = false;
-						// ループを抜ける
-						break;
-					}
-					// インデックスを増加
-					arrayIndex++;
-				}
-				// 加算結果が正しくない場合
-				if (!additionCorrect) {
-					// ループを抜ける
-					break;
-				}
-			}
-			// 要素数が等しくない場合
+		// 行列のサイズが等しいかどうかを確認
+		if (x.length != y.length || x.length != z.length || x[0].length != y[0].length || x[0].length != z[0].length) {
+			// サイズが等しくない場合はフラグをfalseに設定
+			canAdd = false;
+			// 等しい場合
 		} else {
-			// 配列の要素数が等しくないことを示すフラグ変数をfalseに設定
-			equalElements = false;
+			// 行数分繰り返し
+			for (int i = 0; i < x.length; i++) {
+				// 列数分繰り返し
+				for (int j = 0; j < x[i].length; j++) {
+					// 対応する要素の和を計算
+					z[i][j] = x[i][j] + y[i][j];
+				}
+			}
 		}
 
-		// 配列の要素数が等しくない場合
-		if (!equalElements) {
-			// エラーメッセージを出力
-			System.out.println("配列の要素数が等しくないため、加算できません。");
-		}
-
-		// 加算結果が正しい場合
-		if (additionCorrect && equalElements) {
-			// 加算結果が正しいことを示すメッセージを出力
-			System.out.println("加算結果は正しいです。");
-		} else if (equalElements) {
-			// 加算結果が正しくないことを示すメッセージを出力
-			System.out.println("加算結果が正しくありません。");
-		}
-
-		// 配列の要素数が等しく、加算結果が正しい場合はtrue、そうでない場合はfalseを返す
-		return equalElements && additionCorrect;
+		// 加算が可能かどうかを返す
+		return canAdd;
 	}
 
 	/*
 	 * 関数名:printMatrix
-	 * 概要:二次元配列を出力する
-	 * 引数:二次元配列m
+	 * 概要:行列を出力する
+	 * 引数:int型配列[m[][]]
 	 * 戻り値:なし
 	 * 作成者:N.Hagiwara
 	 * 作成日:2024/04/08
 	 */
 	static void printMatrix(int[][] m) {
-		// 行数分繰り返す
+		// 行列の行数だけループ
 		for (int i = 0; i < m.length; i++) {
-			// 列数分繰り返す
+			// 行列の列数だけループ
 			for (int j = 0; j < m[i].length; j++) {
-				// 配列の要素を出力
+				// 要素を出力
 				System.out.print(m[i][j] + " ");
 			}
-			// 改行する
+			// 改行
 			System.out.println();
 		}
 	}
 
 	/*
 	 * 関数名:inputMatrix
-	 * 概要:コンソールから二次元配列の要素数と要素を入力する
-	 * 引数:Scannerオブジェクト、配列の番号、配列の名前
-	 * 戻り値:二次元配列
+	 * 概要:行列の要素を入力する
+	 * 引数:Scannerオブジェクト、行数、列数
+	 * 戻り値:なし
 	 * 作成者:N.Hagiwara
 	 * 作成日:2024/04/08
 	 */
-	static int[][] inputMatrix(Scanner standardInput, int matrixNumber, String matrixName) {
-		// 行を表す変数を定義
-		int arrayRows = 0;
-		// 列を表す変数を定義
-		int arrayCols = 0;
-
-		// 行数の入力を促す
-		System.out.print(matrixNumber + "つ目の配列の行数を入力してください: ");
-		// 行数を受け付け
-		arrayRows = standardInput.nextInt();
-		// 行数が正の整数になるまで再入力を促す
-		while (arrayRows <= 0) {
-			// 正の整数値の入力を促す
-			System.out.print("正の整数を入力してください: ");
-			// 行数の入力を受け付け
-			arrayRows = standardInput.nextInt();
-		}
-
-		// 列数の入力を促す
-		System.out.print(matrixNumber + "つ目の配列の列数を入力してください: ");
-		// 列数を受け付け
-		arrayCols = standardInput.nextInt();
-		// 列数が正の整数になるまで再入力を促す
-		while (arrayCols <= 0) {
-			// 正の整数の入力を促す
-			System.out.print("正の整数を入力してください: ");
-			// 列数の入力を受け付け
-			arrayCols = standardInput.nextInt();
-		}
-
-		// 入力した行数と列数の二次元配列を生成
+	static int[][] inputMatrix(Scanner standardInput, int arrayRows, int arrayCols) {
+		// 指定されたサイズの行列を作成
 		int[][] arrayMatrix = new int[arrayRows][arrayCols];
-
-		// 配列の要素の入力を促す
-		System.out.println(matrixNumber + "つ目の配列の要素を入力してください:");
-		// 行数分繰り返す
+		// 行数だけループ
 		for (int i = 0; i < arrayRows; i++) {
-			// 列数分繰り返す
+			// 列数だけループ
 			for (int j = 0; j < arrayCols; j++) {
-				// 配列の要素の入力を促す
-				System.out.print(matrixName + "[" + i + "][" + j + "] = ");
-				// 配列の要素を受け付け
+				// 要素を入力
 				arrayMatrix[i][j] = standardInput.nextInt();
 			}
 		}
-
-		// 配列を返す
+		// 入力された行列を返す
 		return arrayMatrix;
 	}
 
 	/*
 	 * 関数名:main
 	 * 概要:3つの配列の要素数が等しければ加算を行いtrueを返し
-	 * そうでなければ加算を行わずfalseを返すプログラム
-	 * 引数:なし
+	 * そうでなければ加算を行わずfalseを返す
 	 * 戻り値:なし
 	 * 作成者:N.Hagiwara
 	 * 作成日:2024/04/08
 	 */
 	public static void main(String[] args) {
-		// Scannerオブジェクトを生成
+		// Scannerオブジェクトを作成
 		Scanner standardInput = new Scanner(System.in);
 
-		// 一つ目の配列を受付
-		int[][] firstArray = inputMatrix(standardInput, 1, "firstArray");
+		// 行列aの行数を入力するようメッセージを表示
+		System.out.print("行列aの行数を入力してください: ");
+		// 行列aの行数を入力
+		int firstRows = standardInput.nextInt();
+		// 行列aの列数を入力するようメッセージを表示
+		System.out.print("行列aの列数を入力してください: ");
+		// 行列aの列数を入力
+		int firstCols = standardInput.nextInt();
 
-		// 二つ目の配列を受付
-		int[][] secondArray = inputMatrix(standardInput, 2, "secondArray");
+		// 入力が有効かどうかを示すフラグ
+		boolean validInput = true;
 
-		// 三つ目の二次元配列を生成
-		int[][] thirdArray = new int[firstArray.length][firstArray[0].length];
-
-		// 一つ目の配列の要素数を表示
-		System.out.println("一つ目の配列の要素数: " + (firstArray.length * firstArray[0].length));
-		// 二つ目の配列の要素数を表示
-		System.out.println("二つ目の配列の要素数: " + (secondArray.length * secondArray[0].length));
-
-		// 加算を行い、結果を取得
-		boolean additionSuccessful = addMatrix(firstArray, secondArray, thirdArray);
-
-		// addMatrixメソッドの戻り値を表示
-		System.out.println("addMatrix: " + additionSuccessful);
-
-		// 加算が成功した場合
-		if (additionSuccessful) {
-			// 一つ目の配列タイトル表示
-			System.out.println("一つ目の配列");
-			// 一つ目の配列の中身を表示
-			printMatrix(firstArray);
-			// 二つ目の配列タイトル表示
-			System.out.println("二つ目の配列");
-			// 二つ目の配列の中身を表示
-			printMatrix(secondArray);
-			// 三つ目の配列タイトル表示
-			System.out.println("三つ目の配列(加算結果)");
-			// 三つの配列の中身を表示
-			printMatrix(thirdArray);
-			//加算が成功しなかった場合
-		} else {
-			// 一つ目の配列タイトル表示
-			System.out.println("一つ目の配列");
-			// 一つ目の配列の中身を表示
-			printMatrix(firstArray);
-			// 二つ目の配列タイトル表示
-			System.out.println("二つ目の配列");
-			// 二つ目の配列の中身を表示
-			printMatrix(secondArray);
+		// 行列aの行数と列数が正の整数かどうかを確認
+		if (firstRows <= 0 || firstCols <= 0) {
+			// 正の整数でない場合はメッセージを表示
+			System.out.println("行数と列数は正の整数で入力してください。");
+			// 入力が無効であることを示すためフラグをfalseに設定
+			validInput = false;
 		}
 
-		// Scannerオブジェクトの受付を終了
-		standardInput.close();
+		// 行列aを格納する変数を宣言
+		int[][] firstArray = null;
+		// 入力が有効な場合
+		if (validInput) {
+			// 行列aの要素を入力するようメッセージを表示
+			System.out.println("行列aの要素を入力してください:");
+			// 行列aの要素を入力
+			firstArray = inputMatrix(standardInput, firstRows, firstCols);
+		}
+
+		// 行列bの行数を入力するようメッセージを表示
+		System.out.print("行列bの行数を入力してください: ");
+		// 行列bの行数を入力
+		int secondRows = standardInput.nextInt();
+		// 行列bの列数を入力するようメッセージを表示
+		System.out.print("行列bの列数を入力してください: ");
+		// 行列bの列数を入力
+		int secondCols = standardInput.nextInt();
+
+		// 行列bの行数と列数が正の整数かどうかを確認
+		if (secondRows <= 0 || secondCols <= 0) {
+			// 正の整数でない場合はメッセージを表示
+			System.out.println("行数と列数は正の整数で入力してください。");
+			// 入力が無効であることを示すためフラグをfalseに設定
+			validInput = false;
+		}
+
+		// 行列bを格納する変数を宣言
+		int[][] secondArray = null;
+		// 入力が有効な場合
+		if (validInput) {
+			// 行列bの要素を入力するようメッセージを表示
+			System.out.println("行列bの要素を入力してください:");
+			// 行列bの要素を入力
+			secondArray = inputMatrix(standardInput, secondRows, secondCols);
+		}
+
+		// 入力が有効な場合
+		if (validInput) {
+			// 加算結果を格納する行列cを宣言
+			int[][] thirdArray = new int[firstRows][firstCols];
+
+			// 行列の加算を行う
+			if (addMatrix(firstArray, secondArray, thirdArray)) {
+				// 行列aの文字列を出力
+				System.out.println("行列a");
+				// 行列aを出力
+				printMatrix(firstArray);
+				// 行列bの文字列を出力
+				System.out.println("行列b");
+				// 行列bを出力
+				printMatrix(secondArray);
+				// 行列cの文字列を出力
+				System.out.println("行列c");
+				// 行列cを出力
+				printMatrix(thirdArray);
+				// 行列の加算ができない場合
+			} else {
+				// 加算ができない場合はメッセージを表示
+				System.out.println("行列の加算ができません。行列のサイズが異なります。");
+				// 行列aの文字列を出力
+				System.out.println("行列a");
+				// 行列aを出力
+				printMatrix(firstArray);
+				// 行列bの文字列を出力
+				System.out.println("行列b");
+				// 行列bを出力
+				printMatrix(secondArray);
+			}
+		}
 	}
 }
